fe7f51ed1048538d79f6fcec23656d94e966c0cf
Neeraj Yadav <123739028+NeerajSandal12@users.noreply.github.com>
2023-02-01 16:19:51 +0530

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -3,13 +3,6 @@ import './footer.css'
 const Footer = () => {
   return (
 <>
-{/* <!--FONT AWESOME--> */}
-{/* <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> */}
-
-{/* <!--GOOGLE FONTS-->
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">  */}
 
 {/* <!--footer--> */}
 <footer className="padding_4x">
